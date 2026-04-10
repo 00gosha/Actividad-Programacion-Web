d02f6ca552f94d2d3d3752bf9c0413c0bf175654
Benjamin <benjaminandre.mch@gmail.com>
2026-04-09 21:39:40 -0400

    Add files via upload
    En esta actividad implementamos los conocimientos enseñados en clases, realizando tareas como que aparezcan alertas, mediante un id agregar un mensaje y tambien como imprimir a traves de la consola.

---
+++ Actividad/script.js
@@ -0,0 +1,11 @@
+//El console log sirve para que nos imprima lo que queramos en la consola.
+console.log("Hola Mundo");
+
+//Creamos una alerta que al entrar al sitio web nos aparece en la parte superior.
+alert("Hola Mundo")
+
+//Le damos un mensaje al elemento mediante el id.
+document.getElementById("mensaje").innerText="Catalogo de nuestras herramientas....";
+
+//Para agregarle un mensaje a mi div mediante la id, utilizamos innerHTML a diferencia de el que usamos anteriormente.
+document.getElementById("mensajediv").innerHTML="Caracteristica de....";
